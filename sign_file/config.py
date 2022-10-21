ecff563a3658f1afe9f66988896178a76523efc5
Kirill Zhukov <kzhukov@cloudlinux.com>
2022-10-21 19:39:11 +0200

    ALBS-681 - added .env support for configuration - moved from start.sh to start.py

--- sign_file/config.py
+++ sign_file/config.py
@@ -1,7 +1,8 @@
-from pydantic import BaseSettings
+from email.policy import default
+from pydantic import BaseSettings, Field
 
-GPG_BINARY_DEFAULT = "/usr/local/MacGPG2/bin/gpg2"
-KEYRING_DEFAULT = "/Users/kzhukov/.gnupg/pubring.kbx"
+GPG_BINARY_DEFAULT = "/usr/bin/gpg2"
+KEYRING_DEFAULT = "/home/alt/.gnupg/pubring.kbx"
 MAX_UPLOAD_BYTES_DEFAULT = 100000000
 PASS_DB_DEV_PASS_DEFAULT = ""
 PASS_DB_DEV_MODE_DEFAULT = False
@@ -10,14 +11,29 @@ PGP_KEYS_ID_DEFAULT = ["EF0F6DF0AFE52FD5", ]
 
 
 class Settings(BaseSettings):
+    gpg_binary: str = Field(default=GPG_BINARY_DEFAULT,
+                            description="path to GNU binary",
+                            env="SF_GPG_BINARY")
+    keyring: str = Field(default=KEYRING_DEFAULT,
+                         description="path to PGP keyring database",
+                         env="SF_KEYRING")
+    max_upload_bytes = Field(default=MAX_UPLOAD_BYTES_DEFAULT,
+                             description="max size in bytes for file to sign",
+                             env="SF_SMAX_UPLOAD_BYTES")
+    pass_db_dev_mode: bool = Field(default=PASS_DB_DEV_MODE_DEFAULT,
+                                   env="SF_PASS_DB_DEV_MODE")
+    pass_db_dev_pass: str = Field(default=PASS_DB_DEV_PASS_DEFAULT,
+                                  env="SF_PASS_DB_DEV_PASS")
+    tmp_dir: str = Field(default=TMP_FILE_DIR_DEFAULT,
+                         description="dir to store temp files",
+                         env="SF_TMP_FILE_DIR")
+    pgp_keys: list[str] = Field(default=PGP_KEYS_ID_DEFAULT,
+                                description="list of keyIDs to use",
+                                env="SF_PGP_KEYS_ID")
 
-    gpg_binary: str = GPG_BINARY_DEFAULT
-    keyring: str = KEYRING_DEFAULT
-    max_upload_bytes = MAX_UPLOAD_BYTES_DEFAULT
-    pass_db_dev_pass: str = PASS_DB_DEV_PASS_DEFAULT
-    pass_db_dev_mode: bool = False
-    tmp_dir: str = TMP_FILE_DIR_DEFAULT
-    pgp_keys: list[str] = PGP_KEYS_ID_DEFAULT
+    class Config:
+        case_sensitive = False
+        env_file = '.env'
 
 
 settings = Settings()
--- start.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-source env/bin/activate && uvicorn --workers 4 --host 0.0.0.0 sign_file.app:app
