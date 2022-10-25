31f7a797fabfd5945b8d588ab35c21e5d60f94ed
Kirill Zhukov <kzhukov@cloudlinux.com>
2022-10-25 15:12:13 +0200

    ALBS-681: bugfix with wrong GPG signature for text files

--- sign_file/pgp/pgp.py
+++ sign_file/pgp/pgp.py
@@ -1,3 +1,9 @@
+import aiofiles
+from aiofiles.os import remove
+from fastapi import UploadFile
+import logging
+import plumbum
+import pexpect
 from .pgp_password_db import PGPPasswordDB
 import gnupg
 
@@ -18,8 +24,33 @@ class PGP():
     def list_keys(self):
         return self.__gpg.list_keys()
 
-    def sign(self, keyid: str, fpath: str):
+    def key_exists(self, keyid: str) -> bool:
+        return keyid in self.__pass_db.__keys.keys()
+
+    async def sign(self, keyid: str, file: UploadFile):
         password = self.__pass_db.get_password(keyid)
-        res = self.__gpg.sign_file(
-            fpath, keyid, passphrase=password, detach=True)
-        return res
+        async with aiofiles.tempfile.NamedTemporaryFile(
+                'w', delete=True) as fd:
+            content = await file.read()
+            await fd.write(content.decode(encoding="utf-8"))
+            await fd.flush()
+            # using pgp.sign_file() will result in wrong signature
+            sign_cmd = plumbum.local['gpg'][
+                '--yes', '--detach-sign', '--armor',
+                '--default-key', keyid, fd.name
+            ]
+            out, status = pexpect.run(
+                command=' '.join(sign_cmd.formulate()),
+                events={"Enter passphrase:.*": "{0}\r".format(password)},
+                env={"LC_ALL": "en_US.UTF-8"},
+                timeout=1200,
+                withexitstatus=1,
+            )
+            if status != 0:
+                message = f'gpg failed to sign file, error: {out}'
+                logging.error(message)
+                raise Exception(message)
+        async with aiofiles.open(f'{fd.name}.asc', 'r') as fl:
+            answer = await fl.read()
+        await remove(f'{fd.name}.asc')
+        return answer
