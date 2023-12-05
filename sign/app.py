0632344ecacecbd7663915d87a287245a4f2abbf
Kirill Zhukov <kzhukov@cloudlinux.com>
2023-12-05 16:35:19 +0000

    1. Added SF_ROOT_URL attribute 2. Documentation:   - Fixed curl examples   - Added python client example   - Added info on how to deploy service behind the Nginx

--- sign/app.py
+++ sign/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from sign.api.routes import router
+from sign.config import settings
 import logging
 
 logging.basicConfig(
@@ -7,5 +8,5 @@ logging.basicConfig(
     format="[%(asctime)s] %(levelname)s - %(message)s"
 )
 
-app = FastAPI()
+app = FastAPI(root_path=settings.root_url)
 app.include_router(router)
