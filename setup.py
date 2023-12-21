1ed3ab14bcdd5286345d114c0a434366bf49e262
Kirill Zhukov <kzhukov@cloudlinux.com>
2023-12-21 09:03:01 +0000

    Release 0.1.1: Fixes build-system#159 bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sign',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(include=['sign']),
     install_requires=[
         'python-gnupg == 0.5.0',
