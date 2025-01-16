826da916d9cc9d858bb7cb77c662b779202a3310
Kwaizer <levpambuk@gmail.com>
2025-01-16 00:29:13 +0000

    Add Sentry support

--- sign/app.py
+++ sign/app.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+
 from fastapi import FastAPI
 from sign.api.routes import router
 from sign.config import settings
@@ -7,6 +9,16 @@ logging.basicConfig(
     level="INFO",
     format="[%(asctime)s] %(levelname)s - %(message)s"
 )
+
+if settings.sentry_dsn:
+    sentry_sdk.init(
+        dsn=settings.sentry_dsn,
+        traces_sample_rate=settings.sentry_traces_sample_rate,
+        environment=settings.sentry_environment,
+        ignore_errors=[
+            ConnectionResetError
+        ],
+    )
 
 app = FastAPI(root_path=settings.root_url)
 app.include_router(router)
